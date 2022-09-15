714aed26df43000a2b271f449df08a2587955c0c
jomobrain1 <jomobrain1@gmail.com>
2022-09-15 10:46:45 -0700

    initial commit

---
+++ 10_include_require/01_website_skeleton/index.php
@@ -0,0 +1,11 @@
+<?php include_once('./partials/header.php') ?>
+<!-- You can use include or include_once -->
+<!-- in require if the file is not found then the rest of the code compilation stops and throws an ERROR while in include it throws an error but the code execution continues -->
+
+<!-- require('./partials/headers.php') ?  -->
+
+<h1>This is The Home Page</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ad earum sapiente, facere neque sunt vitae ipsa illo quidem explicabo porro. Excepturi totam molestias impedit! Tenetur repellat non alias deleniti.</p>
+
+
+<?php include_once('./partials/footer.php') ?>
---
+++ 10_include_require/01_website_skeleton/partials/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    Copyright &copy; 2020 jomobrain1
+    <b>This is the Footer</b>
+</footer>
+</body>
+</html>
