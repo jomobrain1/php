3b3511057c59b583bb6bcc99d35f83fd1485e7b1
jomobrain1 <jomobrain1@gmail.com>
2022-09-15 10:04:45 -0700

    initial commit

---
+++ 03_numbers.php
@@ -0,0 +1,25 @@
+<?php
+
+// Declaring numbers
+$a=45;
+$b=6;
+// Arithmetic operations
+echo $a+$b."<br>";
+echo  $a % $b."<br>";
+
+// Assignment with math operators
+// $a +=$b; echo"incRemenT". $a."<br>";
+// Increment operator
+ echo $a++."assignment after <br>";
+ echo ++$a."assignment b4 <br>";
+// Decrement operator
+
+// Number checking functions
+
+// Conversion
+
+// Number functions
+
+// Formatting numbers
+
+// https://www.php.net/manual/en/ref.math.php
