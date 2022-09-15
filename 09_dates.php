d92e95d7fd6c0c6c9342dae75d2da91381e26115
jomobrain1 <jomobrain1@gmail.com>
2022-09-15 10:40:33 -0700

    initial commit

---
+++ 09_dates.php
@@ -0,0 +1,13 @@
+<?php
+
+// Print current date
+echo date('Y-m-d H:i:s').'<br>';
+// Print yesterday
+echo date('Y-m-d H:i:s',time()- 60*60*24).'<br>';
+// Different format: https://www.php.net/manual/en/function.date.php
+echo date('F j Y, H:i:s').'<br>';
+// Print current timestamp
+echo time().'<br>';
+// Parse date: https://www.php.net/manual/en/function.date-parse.php
+
+// Parse date from format: https://www.php.net/manual/en/function.date-parse-from-format.php
