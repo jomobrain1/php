fc7004f0f0fbe157be760683bc1e41c8b6be11b7
jomobrain1 <jomobrain1@gmail.com>
2022-09-15 13:06:25 -0700

    initial commit

---
+++ 13_curl/index.php
@@ -0,0 +1,36 @@
+<?php
+
+$url = 'https://jsonplaceholder.typicode.com/users';
+// Sample example to get data.
+
+$resource=curl_init($url);
+curl_setopt($resource,CURLOPT_RETURNTRANSFER,true);
+$result=curl_exec($resource);
+$code =curl_getinfo($resource,CURLINFO_HTTP_CODE);
+echo '<pre>';
+//echo var_dump($code).'<br>';
+echo '</pre>';
+//echo $result;
+// Get response status code
+
+// set_opt_array
+
+// Post request
+$resource=curl_init();
+$user=[
+    "name"=>"jomobrain1",
+    "username"=>"beast402",
+    "email"=>"jomobrain1@gmail.com"
+];
+
+curl_setopt_array($resource,[
+    CURLOPT_URL=>$url,
+    CURLOPT_RETURNTRANSFER=>true,
+    CURLOPT_POST=>true,
+    CURLOPT_HTTPHEADER=>['content-type:application/json'],
+    CURLOPT_POSTFIELDS=>json_encode($user)
+]);
+
+$result=curl_exec($resource);
+curl_close($resource);
+echo $result;
